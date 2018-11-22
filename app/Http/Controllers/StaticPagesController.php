9a7d1b5a546f758b55e0d97804f78e18e948ac3f
raulzhou <420879586@qq.com>
2018-11-22 14:03:38 +0800

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //
+    public function index(){
+    	return view('static_pages/index');
+    }
+
+    public function help(){
+    	return view('static_pages/help');
+    }
+
+    public function about(){
+    	return view('static_pages/about');
+    }
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title', 'hello-laravel')</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
